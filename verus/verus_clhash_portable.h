d94a3710b60e32c4cba57fd5a80659d4119e1871
Shawn Fitzgerald <sfitzjava@gmail.com>
2022-02-18 19:35:36 -0500

    Initial modifications to support Verushash v2.2 (hash_ver = 6)

---
+++ verus/verus_clhash_portable.h
@@ -0,0 +1,31 @@
+/*
+ * This uses veriations of the clhash algorithm for Verus Coin, licensed
+ * with the Apache-2.0 open source license.
+ * 
+ * Copyright (c) 2018 Michael Toutonghi
+ * Distributed under the Apache 2.0 software license, available in the original form for clhash
+ * here: https://github.com/lemire/clhash/commit/934da700a2a54d8202929a826e2763831bd43cf7#diff-9879d6db96fd29134fc802214163b95a
+ * 
+ * CLHash is a very fast hashing function that uses the
+ * carry-less multiplication and SSE instructions.
+ *
+ * Original CLHash code (C) 2017, 2018 Daniel Lemire and Owen Kaser
+ * Faster 64-bit universal hashing
+ * using carry-less multiplications, Journal of Cryptographic Engineering (to appear)
+ *
+ * Best used on recent x64 processors (Haswell or better).
+ *
+ **/
+
+#include "arm_neon.h"
+//#include "verus_clhash.h"
+
+typedef int32x4_t __m128i;
+typedef unsigned char u_char;
+typedef __m128i u128;
+
+__m128i _mm_setr_epi8_emu(u_char c0, u_char c1, u_char c2, u_char c3, u_char c4, u_char c5, u_char c6, u_char c7, u_char c8, u_char c9, u_char c10, u_char c11, u_char c12, u_char c13, u_char c14, u_char c15);
+
+__m128i _mm_shuffle_epi8_emu(__m128i a, __m128i b);
+__m128i _mm_mulhrs_epi16_emu(__m128i _a, __m128i _b);
+__m128i _mm_clmulepi64_si128_emu(const __m128i &a, const __m128i &b, int imm);
